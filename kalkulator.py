c68a21944b12ac544a8fa7a6ab7b99be8522e849
Yoshia <yoshiapangindaheng026@student.unsrat.ac.id>
2025-11-25 14:35:37 +0800

    Menambahkan fitur perkalian.

--- kalkulator.py
+++ kalkulator.py
@@ -3,8 +3,12 @@ print("Hello world!")
 def tambah(a, b):
     return a + b
 
+def kali(a, b):
+    return a * b
+
 def kurang(a, b):
     return a - b
 
 print(tambah(1, 1))
+print(kali(1, 2))
 print(kurang(1, 1))
